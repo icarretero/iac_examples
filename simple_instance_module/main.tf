f3efd2c8ea8786027c47dc3ab60ba379abedd1d2
Nacho Carretero <nacho@sketch.com>
2021-11-06 19:08:16 +0100

    Add example wiht simple instance using module

---
+++ simple_instance_module/main.tf
@@ -0,0 +1,7 @@
+module "web_server" {
+    source          = "./modules/"
+    instance_type   = "t3.micro"
+    cidr_block      = "10.0.1.0/24"
+    instance_count  = 1
+    tags            = merge(var.default_tags,{ Env = "single-env" })
+}
