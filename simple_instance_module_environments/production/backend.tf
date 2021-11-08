9c707a1d5f8da65e3ce0221bd1dcdfa3cf8c7ec6
Nacho Carretero <nacho@sketch.com>
2021-11-08 09:58:02 +0100

    FIX: forgot to commit backend file

--- simple_instance_module_environments/production/backend.tf
+++ simple_instance_module_environments/production/backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "s3" {
+    # Este podría ser otro bucket para no mezclar nada
+    # bucket = "terraform-devops-prod"
     bucket = "terraform-devops-dev"
-    key = "simple_instance_module_staging.tfstate"
+    key = "simple_instance_module_production.tfstate"
   }
 }
